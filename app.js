0680355cab5051fb351a456ccf86179f20f8fc9b
Atif <79011564+786MohammadAtif786@users.noreply.github.com>
2021-09-12 11:58:14 +0530

    correct spelling

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ app.use(express.static('public'));
 app.use(express.static('views'));
 
 app.listen(3000, () => {
-  console.log(`server is listing.`);
+  console.log(`server is listening on 3000.`);
 });
