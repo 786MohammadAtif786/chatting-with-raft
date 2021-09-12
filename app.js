fff8052d838eebf92200406e8ff47d2a1874ba19
atif chat <79011564+786MohammadAtif786@users.noreply.github.com>
2021-09-12 12:14:37 +0530

    remove middleware app.use(express.static('views'))

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
-app.use(express.static('views'));
 
 app.listen(3000, () => {
   console.log(`server is listening on 3000.`);
